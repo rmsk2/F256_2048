e5746c593f4fbe454e773b71721f5af9d5d622c4
Martin Grap <rmsk2@gmx.de>
2024-01-11 18:54:11 +0100

    Improve help text

--- state_help.asm
+++ state_help.asm
@@ -57,7 +57,13 @@ enterState
     #printString M7, len(M7)
     #locate 0, TXT_Y_START+14
     #printString M8, len(M8)
-    #locate 7, TXT_Y_START+18
+    #locate 0, TXT_Y_START+16
+    #printString M10, len(M10)
+    #locate 0, TXT_Y_START+18
+    #printString M11, len(M11)
+
+
+    #locate 7, TXT_Y_START+22
     #printString M9, len(M9)
 
     rts
@@ -65,14 +71,16 @@ enterState
 leaveState
     rts
 
-M1 .text "This is an implementation of a well known puzzle game in which the player has", $0d
-M2 .text "to create a tile with the value two to the power of eleven or 2048 on a", $0d
-M3 .text "playing field of four by four cells. Use the cursor keys, the joystick in", $0d
-M4 .text "port 1 or an SNES pad in the first socket to move the tiles of all cells", $0d
-M5 .text "up, down, left or right. When two equal tiles 'collide' during that movement", $0d
-M6 .text "they merge into a tile with a value twice that of the original tiles. The", $0d
-M7 .text "game is won if a tile with the value two to the power of eleven is created", $0d
-M8 .text "on the playfield.", $0d
-M9 .text "Press F1 now or during the game to return to the start screen", $0d
+M1  .text "This is an implementation of a well known puzzle game in which the player has", $0d
+M2  .text "to create a tile with the value two to the power of eleven or 2048 on a", $0d
+M3  .text "playing field of four by four cells. Use the cursor keys, the joystick in", $0d
+M4  .text "port 1 or an SNES pad in the first socket to move the tiles of all cells", $0d
+M5  .text "up, down, left or right. When two equal tiles 'collide' during that movement", $0d
+M6  .text "they merge into a tile with a value twice that of the original tiles. The", $0d
+M7  .text "game is won if a tile with the value two to the power of eleven is created", $0d
+M8  .text "on the playfield. Invalid moves are signalled by a beep. After a valid move", $0d
+M10 .text "a new tile with a value two is placed on the playing field. The game ends", $0d
+M11 .text "when no valid moves are left.", $0d
+M9  .text "Press F1 now or during the game to return to the start screen", $0d
 
 .endnamespace
