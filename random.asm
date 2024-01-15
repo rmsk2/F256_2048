0db5d2324510b262aa0150b7d2c7aeef8a00e5cf
Martin Grap <rmsk2@gmx.de>
2024-01-15 19:58:49 +0100

    Make random generator work in F256K and F256 Jr.

--- random.asm
+++ random.asm
@@ -5,15 +5,47 @@ RNG_HI = $D6A5
 RNG_CTRL = $D6A6
 
 RAND_NIBBLES .fill 4
+SEED_VAL_LO .byte 0
+SEED_VAL_HI .byte 0
 
 init
+    jsr kGetTimeStamp
+    lda RTC_BUFFER.centis
+    sta SEED_VAL_LO
+    lda RTC_BUFFER.seconds
+    sta SEED_VAL_HI
+
+    lda $D651
+    eor SEED_VAL_LO
+    sta SEED_VAL_LO
+
+    lda $D659
+    eor SEED_VAL_HI
+    sta SEED_VAL_HI
+
+    clc
+    lda $D01A
+    adc SEED_VAL_LO
+    sta SEED_VAL_LO
+
+    clc
+    lda $D018
+    adc SEED_VAL_LO
+    sta SEED_VAL_LO
+
+    lda SEED_VAL_LO
+    sta RNG_LO
+    lda SEED_VAL_HI
+    sta RNG_HI
+    lda #2
+    sta RNG_CTRL
+
     rts
 
 ; get random 16 bit number in accu and x register
 get
     phx
-    lda RNG_CTRL
-    ora #1
+    lda #1
     sta RNG_CTRL
 _wait
     lda RNG_CTRL
