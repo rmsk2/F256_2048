d5f250faa73d621f600a4dda7671d4339cbdcbbc
Martin Grap <rmsk2@gmx.de>
2024-01-26 18:47:59 +0100

    Add sprite definitions to pgz binary

--- make_pgz.py
+++ make_pgz.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+sprdef_name = "sprdef.bin"
 start_address = 0x2500
 
 def make_24bit_address(addr):
@@ -12,18 +13,24 @@ def make_24bit_address(addr):
 l, h, hh = make_24bit_address(os.path.getsize(sys.argv[1]))
 sl, sh, shh = make_24bit_address(start_address)
 
-print(l, h, hh)
-print(sl, sh, shh)
+sp_l, sp_h, sp_hh = make_24bit_address(os.path.getsize(sprdef_name))
+sp_sl, sp_sh, sp_shh = make_24bit_address(0x20000)
 
 pgz_header = bytes([90, sl, sh, shh, l, h, hh])
+sp_header = bytes([sp_sl, sp_sh, sp_shh, sp_l, sp_h, sp_hh])
 pgz_footer = bytes([sl, sh, shh, 0, 0, 0])
 
 with open(sys.argv[1], "rb") as f:
     data = f.read()
 
+with open(sprdef_name, "rb") as f:
+    sp_data = f.read()
+
 with open(sys.argv[1]+".pgz", "wb") as f:
     f.write(pgz_header)
     f.write(data)
+    f.write(sp_header)
+    f.write(sp_data)
     f.write(pgz_footer)
 
 
--- xpm2t64.py
+++ xpm2t64.py
@@ -1,33 +1,31 @@
 import sys
 
-if len(sys.argv) < 4:
-    print("usage: xpm2t64.py <infile> <outfile> <col_label> <skip_empty>")
-    print()
-    print("if <skip_empty> is present only the lines containing data are")
-    print("written to the output file.")
-    sys.exit()
+def generate_asm(in_file, out_file, col_label):
+    with open(in_file, "r") as f:
+        lines = f.readlines()
 
-with open(sys.argv[1], "r") as f:
-    lines = f.readlines()
+    col = col_label
+    data = lines[5:]
 
-col = sys.argv[3]
-data = lines[5:]
+    with open(out_file, "w") as f:
+        for l in data:
+            res = ".byte "
+            d = list(filter(lambda x: (x == ' ') or (x == '.'), l.rstrip()))
+            
+            for c in d:
+                if c != '.':
+                    res += f"{col}, "
+                else:
+                    res += f"$00, "
+            
+            res = res[:len(res)-2] + '\n'
+            f.write(res)
 
-if len(sys.argv) >= 5:
-    data = data[8:]
-    data = data[:15]
+files = [("2.xpm", "C1"), ("4.xpm", "C2"), ("8.xpm", "C3"), ("16.xpm", "C4"), ("32.xpm", "C5"), 
+         ("64.xpm", "C6"), ("128.xpm", "C7"), ("256.xpm", "C8"), ("512.xpm", "C9"), ("1024.xpm", "CA"), 
+         ("2048.xpm", "CB"), ("4096.xpm", "CC"), ("8192.xpm", "CD")]
 
-with open(sys.argv[2], "w") as f:
-    for l in data:
-        res = ".byte "
-        d = list(filter(lambda x: (x == ' ') or (x == '.'), l.rstrip()))
-        
-        for c in d:
-            if c != '.':
-                res += f"{col}, "
-            else:
-                res += f"$00, "
-        
-        res = res[:len(res)-2] + '\n'
-        f.write(res)
+for i in files:
+    asm_name = (i[0][0:-3]) + "asm"
+    generate_asm(i[0], asm_name, i[1])
     
