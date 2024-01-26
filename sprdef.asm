4be1c49c1c06a7ecf7ff30c1e7f1d338fa4b53d5
Martin Grap <rmsk2@gmx.de>
2024-01-26 18:52:56 +0100

    Add asm file for sprite definitions

---
+++ sprdef.asm
@@ -0,0 +1,55 @@
+* = $8000
+
+C1 = 17
+C2 = 35
+C3 = 62
+C4 = 77
+C5 = 133
+C6 = 145
+C7 = 166
+C8 = 180
+C9 = 192
+CA = 215
+CB = 233
+CC = 240
+CD = 255
+
+
+SPR_2
+.include "2.asm"
+
+SPR_4
+.include "4.asm"
+
+SPR_8
+.include "8.asm"
+
+SPR_16
+.include "16.asm"
+
+SPR_32
+.include "32.asm"
+
+SPR_64
+.include "64.asm"
+
+SPR_128
+.include "128.asm"
+
+SPR_256
+.include "256.asm"
+
+SPR_512
+.include "512.asm"
+
+SPR_1024
+.include "1024.asm"
+
+SPR_2048
+.include "2048.asm"
+
+SPR_4096
+.include "4096.asm"
+
+SPR_8192
+.include "8192.asm"
